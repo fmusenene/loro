612e8ce36683670ad978c53bc95782fdb589349c
unknown <fmusenene@gmail.com>
2025-02-06 20:57:16 +0300

    my first commit

---
+++ header.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="LORO">
+    <meta name="keywords" content="Loanday, unica, creative, html">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>LORO | sLOGAN...</title>
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet">
+
+    <!-- Css Styles -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="css/elegant-icons.css" type="text/css">
+    <link rel="stylesheet" href="css/nice-select.css" type="text/css">
+    <link rel="stylesheet" href="css/magnific-popup.css" type="text/css">
+    <link rel="stylesheet" href="css/jquery-ui.min.css" type="text/css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="css/slicknav.min.css" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <link rel="icon" href="img/logo.png" type="image/x-icon">
+
+</head>
+
+<body>
+    <!-- Page Preloder -->
+    <div id="preloder">
+        <div class="loader"></div>
+    </div>
+
+    <!-- Offcanvas Menu Begin -->
+    <div class="offcanvas-menu-overlay"></div>
+    <div class="offcanvas-menu-wrapper">
+        <div class="offcanvas__search">
+            <i class="fa fa-search search-switch"></i>
+        </div>
+        <div class="offcanvas__logo">
+            <a href="./index.php"><img src="img/logo.png" alt=""></a>
+        </div>
+        <nav class="offcanvas__menu mobile-menu">
+            <ul>
+                <li class="active"><a href="./index.php">Home</a></li>
+                <li><a href="./about.php">About</a></li>
+                <li><a href="./services.php">Services</a></li>
+                <li><a href="./blog.php">Blog</a></li>
+                <li><a href="#">Pages</a>
+                    <ul class="dropdown">
+                        <li><a href="./services.php">Features</a></li>
+                        <li><a href="./services-details.php">Services Details</a></li>
+                        <li><a href="./blog-details.php">Blog Details</a></li>
+                    </ul>
+                </li>
+                <li><a href="./contact.php">Contact</a></li>
+            </ul>
+        </nav>
+        <div id="mobile-menu-wrap"></div>
+        <ul class="offcanvas__widget">
+            <li><i class="fa fa-map-marker"></i> Ggaba Road, Kansanga, Kampala, Opposite KIU</li>
+            <li><i class="fa fa-phone"></i> 256752549509</li>
+            <li><i class="fa fa-envelope"></i> Info@lorocredit.co.ug</li>
+        </ul>
+        <!-- <div class="offcanvas__language">
+            <img src="img/flag.png" alt="">
+            <span>English</span>
+            <i class="fa fa-angle-down"></i> -->
+            <!-- <ul>
+                <li><a href="#">English</a></li>
+                <li><a href="#">Bangla</a></li>
+            </ul> -->
+        <!-- </div> -->
+    </div>
+    <!-- Offcanvas Menu End -->
+
+    <!-- Header Section Begin -->
+    <header class="header">
+        <div class="header__top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-9">
+                        <ul class="header__top__widget">
+                            <li><i class="fa fa-map-marker"></i> Ggaba Road, Kansanga, Kampala, Opposite KIU</li>
+                            <li><i class="fa fa-phone"></i> (+256) 752549509</li>
+                            <li><i class="fa fa-envelope"></i> Info@lorocredit.co.ug</li>
+                        </ul>
+                    </div>
+                    <!-- <div class="col-lg-3">
+                        <div class="header__top__language">
+                            <img src="img/flag.png" alt="">
+                            <span>English</span>
+                            <i class="fa fa-angle-down"></i>
+                            <ul>
+                                <li><a href="#">English</a></li>
+                                <li><a href="#">Bangla</a></li>
+                            </ul>
+                        </div> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3">
+                    <div class="header__logo">
+                        <a href="./index.php"><img src="img/logo.png" alt=""></a>
+                    </div>
+                </div>
+                <div class="col-lg-9">
+                    <div class="header__nav">
+                        <nav class="header__menu">
+                            <ul>
+                                <li class="active"><a href="./index.php">Home</a></li>
+                                <li><a href="./about.php">About</a></li>
+                                <li><a href="./services.php">Services</a></li>
+                                <li><a href="./blog.php">Blog</a></li>
+                                <li><a href="#">Pages</a>
+                                    <ul class="dropdown">
+                                        <li><a href="./services.php">Features</a></li>
+                                        <li><a href="./services-details.php">Services Details</a></li>
+                                        <li><a href="./blog-details.php">Blog Details</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="./contact.php">Contact</a></li>
+                            </ul>
+                        </nav>
+                        <div class="header__search">
+                            <i class="fa fa-search search-switch"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="canvas__open">
+                <span class="fa fa-bars"></span>
+            </div>
+        </div>
+    </header>
+    <!-- Header Section End -->
